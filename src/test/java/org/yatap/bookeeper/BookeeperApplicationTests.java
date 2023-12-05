4a8aec15039c50d768a5234dd00fe496e079ccd0
shlee <aba7500@naver.com>
2023-12-05 18:31:12 +0900

    init

---
+++ src/test/java/org/yatap/bookeeper/BookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yatap.bookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
